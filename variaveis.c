1b537d6adc6bba0f638c5d045c707d3fd85e5024
TiagoLima86 <tiagoyami@gmail.com>
2026-03-25 17:08:55 -0300

    sim

--- variaveis.c
+++ variaveis.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main(){
+    int idade;
+    int quantidade;
+    float altura;
+    double peso;
+    char letra;
+    char nome; 
+/*
+    printf("A idade do %s é: %d\n", nome, idade);
+    printf("A altura é: %.2f\n", altura);
+    printf("A opção é: %c\n", letra);
+*/
+    printf("Qual é sua idade: ");
+    scanf("%d", &idade);
+    printf("A idade é: %d\n", idade);
+
+    printf("Qual sua altura: ");
+    scanf("%f", &altura);
+    printf("A altura é: %f\n", altura);
+
+    printf("Qual seu nome: ");
+    scanf(" %s", nome);
+    printf("O seu nome é: %s", nome);
+}
